c63779abf7b26d6b2f71b4aab61a6b0e2e0bbccd
Arturo <arturogaro@gmail.com>
2023-06-22 16:41:54 -0600

    add cors

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(`process.env.PORT: ${process.env.PORT}`);
-  console.log(`process.env.DB_URL: ${process.env.DB_URL}`);
-  console.log(`process.env.DB_USER: ${process.env.DB_USER}`);
-  console.log(`process.env.DB_PASSWORD: ${process.env.DB_PASSWORD}`);
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['*'],
-    methods: ['GET'],
-  });
+  app.enableCors();
 
   await app.listen(process.env.PORT || 3000);
 }
