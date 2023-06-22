8c22426197cf0725b0fd6210f04d360626d0f95c
Arturo <arturogaro@gmail.com>
2023-06-22 11:55:22 -0600

    fix prots

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(`process.env.PORT: ${process.env.PORT}`);
+  console.log(`process.env.DB_URL: ${process.env.DB_URL}`);
+  console.log(`process.env.DB_USER: ${process.env.DB_USER}`);
+  console.log(`process.env.DB_PASSWORD: ${process.env.DB_PASSWORD}`);
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['*'],
     methods: ['GET'],
   });
-
-  console.log(`process.env.PORT: ${process.env.PORT}`);
-  console.log(`process.env.DB_URL: ${process.env.DB_URL}`);
-  console.log(`process.env.DB_USER: ${process.env.DB_USER}`);
-  console.log(`process.env.DB_PASSWORD: ${process.env.DB_PASSWORD}`);
 
   await app.listen(process.env.PORT || 3000);
 }
