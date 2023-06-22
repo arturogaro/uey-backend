b8c7dc7ae1c2caf31b360ef5bbed6b0c7dc13201
Arturo <arturogaro@gmail.com>
2023-06-22 11:41:50 -0600

    logs

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ async function bootstrap() {
     methods: ['GET'],
   });
 
+  console.log(`process.env.PORT: ${process.env.PORT}`);
+  console.log(`process.env.DB_URL: ${process.env.DB_URL}`);
+  console.log(`process.env.DB_USER: ${process.env.DB_USER}`);
+  console.log(`process.env.DB_PASSWORD: ${process.env.DB_PASSWORD}`);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
