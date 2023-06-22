598a91f1f6ea2aa46c41f989bd2eafb27bab06d5
Arturo <arturogaro@gmail.com>
2023-06-22 11:30:55 -0600

    fix ebs port

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ProductSeeder } from './seeder/seeder';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'https://99ce-2806-2f0-90e1-b04f-58ce-25ab-d421-cc85.ngrok-free.app',
-    ],
+    origin: ['*'],
     methods: ['GET'],
   });
 
-  const productSeeder = app.get(ProductSeeder);
-  await productSeeder.seed();
-
-  await app.listen(3000);
+  await app.listen(8081);
 }
 bootstrap();
